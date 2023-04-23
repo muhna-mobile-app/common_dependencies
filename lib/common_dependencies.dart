b89d38794a73224fabba827164b1ae31d4c2cdfb
kaio-nink <kaionink@gmail.com>
2023-04-23 18:42:52 -0300

    feat: add shared_preferences package

--- lib/common_dependencies.dart
+++ lib/common_dependencies.dart
@@ -3,3 +3,4 @@ library common_dependencies;
 export 'package:dio/dio.dart';
 export 'package:get_it/get_it.dart';
 export 'package:equatable/equatable.dart';
+export 'package:shared_preferences/shared_preferences.dart';
