f24563cfcd7478d6e31f3453ecd607bfd8cba970
kaio-nink <kaionink@gmail.com>
2023-05-24 11:12:25 -0300

    feat: update dio's export and add mobx package

--- lib/common_dependencies.dart
+++ lib/common_dependencies.dart
@@ -1,6 +1,8 @@
 library common_dependencies;
 
-export 'package:dio/dio.dart';
+export 'package:dio/dio.dart' hide Interceptors, Interceptor;
 export 'package:get_it/get_it.dart';
 export 'package:equatable/equatable.dart';
 export 'package:shared_preferences/shared_preferences.dart';
+export 'package:mobx/mobx.dart';
+export 'package:flutter_mobx/flutter_mobx.dart' hide version;
